20cd69610024de021d4cf64a825bb7598fc911f6
poorni-ds <poornibavi@gmail.com>
2026-09-11 22:25:06 +0530

    Add files via upload

--- data.py
+++ data.py
@@ -3,3 +3,5 @@ df = pd.read_csv(
 	r"C:\Users\poorn\Downloads\drive-download-20260825T050739Z-1-001\IDS_ALLCountries_Data.csv"
 	, encoding="latin-1"
 ) #encoding latin as it pops unicoding error -- we have done file encoding
+
+#cleaned CSV -- Drivelink --https://github.com/poorni-ds/International_debt_Analysis
